5a2e3b7dd5f21f496421638512806144fbaf91c9
Michael <m.vilabrera@gmail.com>
2013-08-22 10:13:16 -0400

    MapKit Mods
    Making Modifications to tutorials
    
    Ultimate goal-
    get coordinates from CSV file
    get unique identifier info from XML file
    
    Secondary Goal-
    additional data from TXT/JSON(?) file
    
    Bonus objective-
    parse in PDF data for realtime alerts

---
+++ Map/Map/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Map
+//
+//  Created by Michael on 8/21/13.
+//  Copyright (c) 2013 GivingTree. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+@interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet MKMapView *mapView;
+
+@end
---
+++ Map/Map/ViewController.m
@@ -0,0 +1,45 @@
+//
+//  ViewController.m
+//  Map
+//
+//  Created by Michael on 8/21/13.
+//  Copyright (c) 2013 GivingTree. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <MapKit/MapKit.h>
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+    
+    
+    // all these options give you more control or customization for the mapView
+    // i wonder if (IBAction) function still applies in this mode, and how
+    MKMapView *mapView = [[MKMapView alloc] initWithFrame:CGRectMake(0, 0, 320, 480)];
+    // MKMapView - instance created         initWithFrame- create it to be as big as a Rect
+    // *mapView - MKMapView object          CGRectMake(start at upper left to lower right(x,y,x+,y+)
+    [mapView setMapType: 0]; // 0 - standard, 1 - satellite, 2 - hybrid
+    [mapView setZoomEnabled:YES]; // toggleable
+    [mapView setScrollEnabled:YES]; // toggleable
+    [mapView setShowsUserLocation:YES]; // tobbleable
+    [mapView setShouldGroupAccessibilityChildren:YES]; // i don't even know what this does and its toggleable
+    
+    [self.view addSubview:mapView]; // must add mapView as Subview
+    
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
